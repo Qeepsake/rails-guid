90fe70bfca8dda8dd2f7c164dacab46be41e5e74
Luke Farrell <lukebrandonfarrell@gmail.com>
2022-09-26 15:22:15 +0200

    feat(api): added 'stripTimestamp' to support handling partitioned IDs

--- lib/extract-rails-id.d.ts
+++ lib/extract-rails-id.d.ts
@@ -5,8 +5,9 @@
  * then it will be returned 55587
  *
  * @param gid - gid://qeepsake-rails/Model/55587
+ * @param stripTimestamp - whether to strip the timestamp from the ID (i.e. %2C2022-09-26+13%3A08%3A27+UTC)
  *
  * @returns 55587
  */
-export declare function extractRailsId(gid: string): string;
-export declare function extractRailsId(gid?: string | null): string | null;
+export declare function extractRailsId(gid: string, stripTimestamp?: boolean): string;
+export declare function extractRailsId(gid?: string | null, stripTimestamp?: boolean): string | null;
--- src/extract-rails-id.ts
+++ src/extract-rails-id.ts
@@ -5,15 +5,27 @@
  * then it will be returned 55587
  *
  * @param gid - gid://qeepsake-rails/Model/55587
+ * @param stripTimestamp - whether to strip the timestamp from the ID (i.e. %2C2022-09-26+13%3A08%3A27+UTC)
  *
  * @returns 55587
  */
-export function extractRailsId(gid: string): string
-export function extractRailsId(gid?: string | null): string | null
-export function extractRailsId(gid?: string | null): string | null {
+export function extractRailsId(gid: string, stripTimestamp?: boolean): string
+export function extractRailsId(
+  gid?: string | null,
+  stripTimestamp?: boolean,
+): string | null
+export function extractRailsId(
+  gid?: string | null,
+  stripTimestamp = false,
+): string | null {
   if (typeof gid === 'string' && gid?.indexOf('/') != -1) {
     const gidComponents = gid.split('/')
-    const extractedId = gidComponents[gidComponents.length - 1]
+    let extractedId = gidComponents[gidComponents.length - 1]
+
+    // Strips the timestamp (%2C2022-09-26+13%3A08%3A27+UTC)) from the ID
+    if (stripTimestamp && extractedId?.includes('%')) {
+      extractedId = extractedId.split('%')[0]
+    }
 
     if (extractedId) {
       return extractedId
