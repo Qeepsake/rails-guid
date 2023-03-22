f5e071574d18db9cbdf969aa992ddfc91e8bef3a
Luke Farrell <lukebrandonfarrell@gmail.com>
2023-03-22 19:43:02 +0100

    fix(type): added a new type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 export { extractRailsId } from './extract-rails-id';
 export { createRailsId } from './create-rails-id';
 export { isExtractedRailsId } from './is-extracted-rails-id';
+export declare type ModelId = string | number;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { extractRailsId } from './extract-rails-id'
 export { createRailsId } from './create-rails-id'
 export { isExtractedRailsId } from './is-extracted-rails-id'
+
+export type ModelId = string | number
